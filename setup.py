092b00dc5152c8c73009f71ad150c568d9043f4f
debanjan <deepanjan@ineuron.ai>
2021-12-05 16:28:25 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Debanjan",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DebanjanCoder/bacd",
+    author_email="deepanjan@ineuron.ai",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ])
